9cf00611c7e54d1e985e9407ae61c8dbf20c8cfa
gabriel <andreyalth@gmail.com>
2025-08-26 15:53:22 -0600

    add conection to redis

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,16 @@
-console.log("hello world")
+import { Queue, Worker } from 'bullmq'
+import redisConnection from './redis-conection'
+
+
+const videoEncodingQueue = new Queue('video encoding', {
+    connection: redisConnection
+})
+
+const tasksQueue = new Queue('tasks', {
+    connection: redisConnection
+})
+
+videoEncodingQueue.add('video encoding', { videoId: 1 })
+tasksQueue.add('tasks', { task: 'task', description: 'task to handle' })
+
+console.log(videoEncodingQueue)
