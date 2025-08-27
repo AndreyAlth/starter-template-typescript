dce08e46ae1f945c51ee2c36a8544aa71d88f7df
gabriel <andreyalth@gmail.com>
2025-08-27 13:38:24 -0600

    add config to consumer and producer

--- src/consumer.ts
+++ src/consumer.ts
@@ -1,11 +1,11 @@
-import { Worker } from 'bullmq'
+import { Worker, Job } from 'bullmq'
 import redisConnection from './redis-conection'
 
 class Consumer {
     private worker: Worker
     constructor(name: string) {
-        this.worker = new Worker(name, async (job) => {
-            console.log(job, 'job processed')
+        this.worker = new Worker(name, async (job: Job) => {
+            console.log(job.name, 'job processed')
         }, { connection: redisConnection })
     }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 import express from 'express'
 import Producer from './producer'
+import dotenv from 'dotenv'
+dotenv.config()
 
 const app = express()
 const PORT = process.env.PORT || 3000
@@ -9,8 +11,8 @@ app.use(express.json())
 const producer = new Producer('test')
 
 app.post('/add-job', async (req, res) => {
-    const { name, data } = req.body
-    const job = await producer.addJob(name, data)
+    const { data } = req.body
+    const job = await producer.addJob('test', data)
     res.json({ msg: 'job added', job })
 })
 
