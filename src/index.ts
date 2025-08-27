180608cc513155eb68dcbf8c63985df6c04f4309
gabriel <andreyalth@gmail.com>
2025-08-27 10:41:01 -0600

    add redis config file

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,24 @@
-import { Queue, Worker } from 'bullmq'
+import { Queue, Worker, } from 'bullmq'
 import redisConnection from './redis-conection'
 
 
-const videoEncodingQueue = new Queue('video encoding', {
-    connection: redisConnection
-})
 
-const tasksQueue = new Queue('tasks', {
-    connection: redisConnection
-})
+// const videoEncodingQueue = new Queue('video encoding', {
+//     connection: redisConnection
+// })
 
-videoEncodingQueue.add('video encoding', { videoId: 1 })
-tasksQueue.add('tasks', { task: 'task', description: 'task to handle' })
+// const tasksQueue = new Queue('tasks', {
+//     connection: redisConnection
+// })
 
-console.log(videoEncodingQueue)
+// videoEncodingQueue.add('video encoding', { videoId: 1 })
+// tasksQueue.add('tasks', { task: 'task', description: 'task to handle' })
+
+// console.log(videoEncodingQueue)
+
+
+const worker = new Worker('video encoding', async (job) => {
+    console.log(job)
+}, { connection: redisConnection })
+
+// worker.run()
--- src/redis-conection.ts
+++ src/redis-conection.ts
@@ -3,6 +3,7 @@ import Redis from 'ioredis'
 const redisConfig = {
   port: 6379,
   host: '127.0.0.1',
+  maxRetriesPerRequest: null,
 };
 
 const redisConnection = new Redis(redisConfig);
