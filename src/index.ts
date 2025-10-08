81d144e7d7f3cbe087117db4f8e69e5047b8cb73
gabriel <andreyalth@gmail.com>
2025-10-07 21:05:55 -0600

    refactor: remove Redis and BullMQ queue implementation and dependencies

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,4 @@
 import express from 'express'
-import Producer from './producer'
 import dotenv from 'dotenv'
 dotenv.config()
 
@@ -8,13 +7,6 @@ const PORT = process.env.PORT || 3000
 
 app.use(express.json())
 
-const producer = new Producer('test')
-
-app.post('/add-job', async (req, res) => {
-    const { data } = req.body
-    const job = await producer.addJob('test', data)
-    res.json({ msg: 'job added', job })
-})
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
